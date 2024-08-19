0a16263d0ad91c4e53efdadb585329c6c529f707
rockydey077 <dev.rocky@jsencoder.com>
2024-08-19 18:21:54 +0600

    fixed padding

--- src/layout/Dashboard/Dashboard.jsx
+++ src/layout/Dashboard/Dashboard.jsx
@@ -6,12 +6,12 @@ const Dashboard = () => {
   const [hideSidebar, setHideSidebar] = useState(false);
 
   return (
-    <div className='grid grid-cols-12 p-3 gap-3'>
+    <div className='grid grid-cols-12 lg:p-3 gap-3'>
       <Sidebar hideSidebar={hideSidebar} setHideSidebar={setHideSidebar} />
       <div
         className={`${
           hideSidebar ? "col-span-12" : "col-span-10"
-        } w-full bg-gray-100 rounded-lg h-[calc(100vh-24px)] overflow-y-auto`}>
+        } w-full bg-gray-100 rounded-lg h-[calc(100vh-24px)] overflow-y-auto px-5 py-3 lg:px-0 lg:py-0`}>
         <Outlet hideSidebar={hideSidebar} />
       </div>
     </div>
